d425243d7372864ee8f4530cf1bdfaa51ebd140d
Purushothaman-source <Purushoth99jun@gmail.com>
2020-12-19 20:34:31 +0530

    ADDED : created board

--- TicTacToe.java
+++ TicTacToe.java
@@ -2,6 +2,14 @@
 public class TicTacToe {
     //main method
     public static void main(String[] args) {
-        System.out.println("welcome to tic tac toe game");
+        char[] board=createBoard();//calling createBoard method
+
+    }
+    private static char[] createBoard(){   //UC 1
+        char[] board=new char[10];  //initialize board to null
+        for (int i=1;i< board.length;i++){
+            board[i]=' ';
+        }
+        return board;  //return board
     }
 }
