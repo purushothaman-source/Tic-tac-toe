3f1754bb610254d560ff7eda34dbcd0c92818849
Purushothaman-source <Purushoth99jun@gmail.com>
2020-12-19 20:47:47 +0530

    ADDED : showBoard method

--- TicTacToe.java
+++ TicTacToe.java
@@ -7,6 +7,7 @@ public class TicTacToe {
         Scanner userInput=new Scanner(System.in);
         char userLetter =chooseUserLetter(userInput);
         char computeLetter =(userLetter == 'X')?'0':'x';
+        showBoard(board);
     }
     private static char[] createBoard(){   //UC 1
         char[] board=new char[10];
@@ -18,6 +19,19 @@ public class TicTacToe {
     private static char chooseUserLetter(Scanner userInput){  //UC 2
         System.out.println("choose your letter: ");
         return userInput.next().toUpperCase().charAt(0);
+    }
+    public static void showBoard(char[] Board)
+    {
+        System.out.println("");
+        for(int i =1 ; i<Board.length ;i++)
+        {
+            System.out.print("|_"+i+Board[i] + "_|");
+            if(i%3==0)
+            {
+                System.out.print(System.lineSeparator());
+                System.out.println("--------------------");
+            }
+        }
     }
 }
 
