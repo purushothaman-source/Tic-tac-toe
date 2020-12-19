072b3a9c6c1743b489d3fac189ca616aa36a4e29
Purushothaman-source <Purushoth99jun@gmail.com>
2020-12-19 20:42:04 +0530

    ADDED:allow player to input

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,15 +1,26 @@
-//main class
+import java.util.Scanner;
+
 public class TicTacToe {
     //main method
     public static void main(String[] args) {
-        char[] board=createBoard();//calling createBoard method
-
+        char[] board=createBoard();
+        Scanner userInput=new Scanner(System.in);
+        char userLetter =chooseUserLetter(userInput);
+        char computeLetter =(userLetter == 'X')?'0':'x';
     }
     private static char[] createBoard(){   //UC 1
-        char[] board=new char[10];  //initialize board to null
+        char[] board=new char[10];
         for (int i=1;i< board.length;i++){
             board[i]=' ';
         }
-        return board;  //return board
+        return board;
+    }
+    private static char chooseUserLetter(Scanner userInput){  //UC 2
+        System.out.println("choose your letter: ");
+        return userInput.next().toUpperCase().charAt(0);
     }
 }
+
+
+
+
